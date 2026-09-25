4b49a5405a4642c949bcc9891ced8bdd70ecf8a3
A-Alsaffaf <ahmedy.saffaf@gmail.com>
2026-09-26 02:22:20 +0300

    feat(learning): read available learning entries in all-learnings page, create routes for details and edit pages(#6)

--- routes/learning.routes.js
+++ routes/learning.routes.js
@@ -2,15 +2,28 @@ const router = require('express').Router()
 const Learning = require('../models/Learning')
 const Job = require('../models/Job')
 const isSignedIn = require('../middleware/is-signed-in')
+const { find } = require('../models/Todo')
 
-router.get('/', (req,res) => {
-    res.render('../views/learnings/all-learnings.ejs')
+
+// page render routes
+router.get('/', isSignedIn, async (req,res) => {
+    const allLearnings = await Learning.find({owner: req.session.user._id, isDeleted: false}).populate('linkedJobs')
+    res.render('../views/learnings/all-learnings.ejs', {allLearnings})
 })
 
 router.get('/new', async (req,res) => {
     const areaEnums = Learning.schema.path('area').enumValues
     const availableJobs = await Job.find({owner: req.session.user._id})
     res.render('../views/learnings/create-learning.ejs', {areaEnums, availableJobs})
+})
+
+router.get('/:learningId', async (req,res) => {
+    const foundLearning = await Learning.findById(req.params.learningId)
+    res.render('../views/learnings/learning-details.ejs', {foundLearning})
+})
+
+router.get('/:learningId/edit', async (req,res) => {
+    const updatedLearning = await Learning.findById(req.params.learningId)
 })
 
 router.post('/', isSignedIn , async (req,res) => {
@@ -25,6 +38,8 @@ router.post('/', isSignedIn , async (req,res) => {
 
     res.redirect('/learnings')
 })
+
+
 
 
 module.exports = router
